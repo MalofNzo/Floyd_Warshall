f75a5108de8a06dcf82102c5010d30081110aade
MalofNzo <enzomaxx@gmail.com>
2017-12-25 10:44:19 +0800

    Floyd完成，finished

--- main.cpp
+++ main.cpp
@@ -21,27 +21,43 @@ int Input_Graph(Graph &m,p &P,d &D){
     cin>>m.vexnum>>m.edgenum;
     for(int i = 0;i<m.vexnum;i++){
         for(int j = 0;j<m.vexnum;j++){
-            P[i][j] = j;
             if(i==j)
-                D[i][j] = 0;
+                m.edge[i][j] = 0;
             else
-                D[i][j] = INFINITY;
+                m.edge[i][j] = INFINITY;
         }
     }
     for(int i = 0;i<m.edgenum;i++){
+        cout<<"Input x to y ,weight :";
         cin>>x>>y>>z;
-        D[x][y] = z;
+        m.edge[x][y] = z;
+    }
+    for(int i = 0; i < m.vexnum; i++)
+    {
+        for(int j = i; j < m.vexnum; j++)
+        {
+            m.edge[j][i] = m.edge[i][j];
+        }
     }
     return 0;
 }
 
 int Floyd_warshall(Graph &m,p &P,d &D){
-    p ptemp;
-    d dtemp;
     int i,j,k;
     for(i = 0;i<m.vexnum;i++){
         for(j =0;j<m.vexnum;j++){
-            dtemp[i][j] = D[i][j];
+            D[i][j] = m.edge[i][j];
+            P[i][j]=j;
+        }
+    }
+    for(k=0;k<m.vexnum;k++){
+        for(i=0;i<m.vexnum;i++){
+            for(j=0;j<m.vexnum;j++){
+                if(D[i][j]>D[i][k]+D[k][j]){
+                    D[i][j]=D[i][k]+D[k][j];
+                    P[i][j]=P[i][k];
+                }
+            }
         }
     }
 
@@ -55,5 +71,21 @@ int main() {
     p P;
     d D;
     Input_Graph(m,P,D);
+    Floyd_warshall(m,P,D);
+    int temp;
+    for(int i =0;i<m.vexnum;i++){
+        for(int j=i+1;j<m.vexnum;j++){
+            cout<<i<<" to "<<j<<":";
+            cout<<"Distance : "<<D[i][j];
+            cout<<" Path "<<i<<' ';
+            temp = P[i][j];
+            while (temp != j)
+            {
+                cout << "- " << temp << " ";
+                temp = P[temp][j];
+            }
+            cout << "- " << j << endl;
+        }
+    }
     return 0;
 }
